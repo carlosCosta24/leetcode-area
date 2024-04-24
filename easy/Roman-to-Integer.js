5ef18626600588bbffd5d16e68f26f7c6f492e26
M0lt0 <bodymgahed@yahoo.com>
2024-04-24 20:18:08 -0300

    easy challenge n:3

--- easy/Roman-to-Integer.js
+++ easy/Roman-to-Integer.js
@@ -1,12 +1,31 @@
 let romanToInt = (s) => {
-    let symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-    let number = [1, 5, 10, 50, 100, 500, 1000]
-    for (let y = 0; y < symbols.length; y++) {
+    let symbols = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+    let result = 0
 
-        if (s.toUpperCase() != y) {
+
+    for (o = 0; o < s.length; o++) {
+
+        let current = symbols[s[o]]
+        let next = symbols[s[o + 1]]
+        if (current === undefined) {
             console.log('please enter a valid symbol \'I, V, X, L, C, D, M\' ')
+            return
         }
+
+        else if (next !== undefined && current < next) {
+            result += next - current
+            o++
+        } else {
+            result += current
+        }
+
+        result += current
+
     }
+    console.log(result)
+    return result
+
+
 };
 
-romanToInt("o")
+romanToInt("iii")
